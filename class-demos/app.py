bbfbe6c9af35c389d4491eed361916c221cbbdee
Daniil Dreiman <ddreyman1@gmail.com>
2023-06-24 12:42:45 -0400

    Initial commit with basic project structure

---
+++ class-demos/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://danield@localhost:5432/example'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+  __tablename__ = 'persons'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+  person = Person.query.first()
+  return 'Hello ' + person.name
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0")
+# FLASK_DEBUG=true flask run
